2d4a72871344de1f851ab977c0586d773bca3e0c
Jort Willemsen <jort@maurice-nienke.nl>
2025-12-10 14:33:18 +0100

    fix: self-spotted pr issues

--- lib/index.ts
+++ lib/index.ts
@@ -2,7 +2,6 @@ import './styles/styles.scss';
 import { SeButton } from "./components/SeButton.js";
 export { SeButton };
 
-
 // https://lit.dev/docs/components/defining/#typescript-typings
 declare global {
   interface HTMLElementTagNameMap {
