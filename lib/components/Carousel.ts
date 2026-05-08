ad67c990fd24a5c4052220d4a920e2fb2c20eee4
Job Vonk <sticcpistt@icloud.com>
2026-05-08 23:15:21 +0200

    Scaffold new carousel element

---
+++ lib/components/Carousel.ts
@@ -0,0 +1,11 @@
+import { css, html, LitElement } from "lit";
+import { customElement } from "lit/decorators.js";
+
+@customElement("se-carousel")
+export class Carousel extends LitElement {
+  static styles = css``;
+
+  protected render() {
+    return html`Carousel works!`;
+  }
+}
