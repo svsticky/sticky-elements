f4ef106e3f07a0705dd9567ec0c6134fba08ba6f
Job Vonk <sticcpistt@icloud.com>
2026-05-08 18:01:50 +0200

    Made the nav bar responsive

--- lib/components/Nav.ts
+++ lib/components/Nav.ts
@@ -1,6 +1,7 @@
 import { css, html, LitElement } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
 import { classMap } from "lit/directives/class-map.js";
+import { styleMap } from "lit/directives/style-map.js";
 
 type NavLink = { title: string, to: string, type?: "button" };
 // Not recursive, since the SeNav element only supports menus one layer deep.
@@ -28,40 +29,70 @@ export class SeNav extends LitElement {
       height: 100%;
     }
 
-    ul { list-style-type: none; }
-
-    .nav-items {
-      display: flex;
-      flex-direction: row;
-      gap: 2rem;
-      height: 100%;
-      padding: 0;
-      margin: 0;
-    }
-
-    .se-menu-item {
+    .bars {
       color: white;
+      font-size: 2rem;
       margin: auto 0;
-      position: relative
+      cursor: pointer;
     }
 
-    .se-menu-item:not(*:has(.se-menu-button)) a:hover {
-      text-decoration: underline;
+    .icon { margin-left: 0.25rem; }
+
+    ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+    }
+
+    @media (width < 54rem) {
+      .nav-items {
+        display: flex;
+        position: absolute;
+        top: 5rem;
+        left: 0;
+        height: calc(100vh - 9rem);
+        overflow-y: auto;
+        width: calc(100vw - 3rem);
+        background: var(--board-colour);
+        z-index: 10;
+        flex-direction: column;
+        gap: 1rem;
+        padding: 2rem 1.5rem;
+      }
+
+      .nav-hidden { display: none!important; }
+
+      .se-menu-item {
+        font-size: 2rem;
+        color: white;
+      }
+
+      .submenu {
+        position: relative;
+        display: none;
+        flex-direction: column;
+        gap: 1.5rem;
+        padding: 1.5rem 0 1.5rem 2rem;
+      }
+
+      .submenu::before {
+        content: "";
+        height: 90%;
+        width: 4px;
+        background: white;
+        border-radius: 2px;
+        position: absolute;
+        left: .5rem;
+        top: 5%;
+      }
     }
 
     .se-menu-item a {
       color: inherit;
       text-decoration: none;
     }
-
-    .se-menu-button {
-      background: white;
-      border-radius: 0.5rem;
-      padding: 0.5rem 1rem;
-      color: var(--board-colour, black)!important;
-    }
-    .se-menu-button:hover {
-      background: #f1f1f1;
+    .se-menu-item a:hover {
+      text-decoration: underline;
     }
 
     .se-menu-opener {
@@ -70,30 +101,11 @@ export class SeNav extends LitElement {
       user-select: none;
     }
 
-    .submenu {
-      display: none;
-      position: absolute;
-      top: 1.5rem;
-      left: 50%;
-      transform: translateX(-50%);
-      flex-direction: column;
-      background: white;
-      border: 1px solid #999;
-      border-radius: 0.25rem;
-      text-align: center;
-      z-index: 10;
-      padding: 0 1rem;
-      color: black;
-    }
 
     .opened-menu {
       display: flex!important;
     }
 
-    .submenu-item {
-      padding: 0.5rem 0;
-      border-bottom: 1px solid #999;
-    }
     .submenu-item:hover {
       text-decoration: underline;
     }
@@ -101,14 +113,74 @@ export class SeNav extends LitElement {
       border: none;
     }
 
-    .icon { font-size: 0.875rem; margin-left: 0.25rem; }
+    @media (width >= 54rem) {
+      .bars { display: none; }
+
+      .nav-items {
+        display: flex;
+        flex-direction: row;
+        gap: 2rem;
+        height: 100%;
+        padding: 0;
+        margin: 0;
+      }
+
+      .nav-hidden { display: flex!important; }
+
+      .se-menu-item {
+        color: white;
+        margin: auto 0;
+        position: relative;
+        font-size: 1rem;
+      }
+
+      .se-menu-button {
+        background: white;
+        border-radius: 0.5rem;
+        padding: 0.5rem 1rem;
+        color: var(--board-colour, black)!important;
+      }
+      .se-menu-button:hover {
+        background: #f1f1f1;
+      }
+
+      .se-menu-item:not(*:has(.se-menu-button)) a:hover {
+        text-decoration: underline;
+      }
+
+      .icon { font-size: 0.875rem; }
+
+      .submenu {
+        display: none;
+        position: absolute;
+        top: 1.5rem;
+        left: 50%;
+        transform: translateX(-50%);
+        flex-direction: column;
+        background: white;
+        border: 1px solid #999;
+        border-radius: 0.25rem;
+        text-align: center;
+        z-index: 10;
+        padding: 0 1rem;
+        color: black;
+      }
+
+      .submenu-item {
+        padding: 0.5rem 0;
+        border-bottom: 1px solid #999;
+      }
+    }
   `;
 
   @property({ type: Array })
   menus: NavItem[] = [];
 
-  @property({ attribute: false })
+  @state()
   private opened_menu = -1;
+
+  @state()
+  private mobile_opened = false;
 
   private handle_click = (e: Event) => {
     if (this.opened_menu !== -1) {
@@ -149,9 +221,9 @@ export class SeNav extends LitElement {
     } else {
       return html`
         <li data-nav-index=${index} class="se-menu-item">
-          <a @click=${() => this.opened_menu = index} class="se-menu-item se-menu-opener">
+          <a @click=${() => this.opened_menu = this.opened_menu === index ? -1 : index} class="se-menu-item se-menu-opener">
             ${item.title}
-            <i class="fa-solid fa-caret-down"></i>
+            <i class=${classMap({ 'icon fa-solid': true, 'fa-caret-down': this.opened_menu !== index, 'fa-caret-up': this.opened_menu === index })}></i>
           </a>
           <ul class=${classMap({ 'submenu': true, 'opened-menu': this.opened_menu === index })}>
             ${item.submenu.map(sub_item => html`
@@ -179,7 +251,8 @@ export class SeNav extends LitElement {
           alt="SV Sticky's logo"
           class="logo"
         />
-        <ul class="nav-items">${this.menus.map(this.render_menu_item.bind(this))}</ul>
+        <i @click=${() => this.mobile_opened = !this.mobile_opened} class="bars fa-solid fa-bars"></i>
+        <ul class=${classMap({ 'nav-items': true, 'nav-hidden': !this.mobile_opened })}>${this.menus.map(this.render_menu_item.bind(this))}</ul>
       </nav>
     `;
   }
